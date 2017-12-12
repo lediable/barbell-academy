66634af6a6565f20bfeb52d4bf08d2320557ca69
lediable <hiretto@gmail.com>
2017-12-12 23:21:40 +0100

    Add files via upload
    Initial commit

---
+++ home.php
@@ -0,0 +1,146 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Barbell Academy</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="landing">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+                                            <h1><a href="index.html">Barbell Academy</a></h1>                                                          
+						<nav id="nav">
+							<ul>
+                                                                <li class="special">
+                                                                    <a href="login.php">Login</a>
+								</li>
+                                                                <li class="special">
+                                                                    <a href="registration.php">Sign Up</a>
+								</li>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Menu</span></a>
+									<div id="menu">
+										<ul>
+                                                                                    <li><a href="home.php">Home</a></li>
+                                                                                    <li><a href="instructor.php">Trainer</a></li>
+                                                                                    <li><a href="about.php">About</a></li>
+                                                                                    <li><a href="contact.php">Contact</a></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<h2>Barbell Academy</h2>
+							<p>TAKE YOUR FIRST STEP TOWARDS STRENGHT
+							</p>
+							<ul class="actions">
+                                                            <li><a class="button special" href="registration.php">Try it today!</a></li>
+							</ul>
+						</div>
+						<a href="#one" class="more scrolly">Learn More</a>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style1 special">
+						<div class="inner">
+							<header class="major">
+								<h2>Barbell Academy<br />
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br>
+								sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+							</header>
+						</div>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper alt style2">
+						<section class="spotlight">
+                                                    <div class="image"><img src="images/package.jpg" alt="" /></div><div class="content">
+								<h2>Choose your training</h2>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                                                                <h3>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
+							</div>
+						</section>
+						<section class="spotlight">
+                                                    <div class="image"><img src="images/instructor.jpg" alt="" /></div><div class="content">
+								<h2>Find your trainer</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                                <h3>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
+                                                    </div>
+						</section>
+						
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style3 special">
+						<div class="inner">
+							<header class="major">
+								<h2>Personal Account</h2>
+                                                                <p>Register with our website today & get your personal member account</p>
+							</header>
+							<ul class="features">
+								
+								<li class="icon fa-laptop">
+									<h3>Trainer</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+								</li>
+								<li class="icon fa-flag-o">
+									<h3>Training</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+								</li>
+								<li class="icon fa-heart-o">
+									<h3>Strenght</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+								</li>
+								<li class="icon fa-paper-plane-o">
+									<h3>Contact</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+								</li>
+                                                                
+							</ul>
+						</div>
+					</section>
+
+				<!-- CTA -->
+					<section id="cta" class="wrapper style4">
+						<div class="inner">
+							<header>
+								<h2>Register Today</h2>
+								<p>Register for you gym membership & personal account with just a click!</p>
+							</header>
+							<ul class="actions vertical">
+                                                            <li><a class="button special" href="registration.php">Sign Up</a></li>
+								<li><a href="#" class="button fit">Contact Us</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="copyright">
+							<li>&copy; Barbell Academy</li><li>2017</li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
